bd5f7b293695db6350511ee4ec570be5a40df3cc
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-08-26 16:26:17 -0300

    Remover print usado pra debug

--- TelegramBot/zabbix_graph.py
+++ TelegramBot/zabbix_graph.py
@@ -6,7 +6,6 @@ from dados_conta import ZABBIX_SERVER, ZABBIX_USER, ZABBIX_PWD
 def get_graph(graph_id):
     login_cmd = f'wget  -q -O /dev/null --save-cookies=./cookies_file --keep-session-cookies "{ZABBIX_SERVER}/index.php?login=1&form=1&name={ZABBIX_USER}&password={ZABBIX_PWD}&enter=ENTER"'
     graph_cmd = f'wget -q --post-data="" -O ./imagem.png --load-cookies=./cookies_file "{ZABBIX_SERVER}chart2.php?graphid={graph_id}&from=now-7d&to=now"'
-    print(graph_cmd)
     os.system(login_cmd)
     sleep(3)
     os.system(graph_cmd)
