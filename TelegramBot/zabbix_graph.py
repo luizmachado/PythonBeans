de9533680e7df2933053320f731c409f1fb05f0f
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-08-25 23:11:13 -0300

    Removing garbage

--- TelegramBot/zabbix_graph.py
+++ TelegramBot/zabbix_graph.py
@@ -5,7 +5,7 @@ from dados_conta import ZABBIX_SERVER, ZABBIX_USER, ZABBIX_PWD
 
 def get_graph(graph_id):
     login_cmd = f'wget  -q -O /dev/null --save-cookies=./cookies_file --keep-session-cookies "{ZABBIX_SERVER}/index.php?login=1&form=1&name={ZABBIX_USER}&password={ZABBIX_PWD}&enter=ENTER"'
-    graph_cmd = f'wget --post-data="" -O ./imagem.png --load-cookies=./cookies_file "http://192.168.5.22/zabbix/chart2.php?graphid={graph_id}&from=now-7d&to=now"'
+    graph_cmd = f'wget --post-data="" -O ./imagem.png --load-cookies=./cookies_file "{ZABBIX_SERVER}chart2.php?graphid={graph_id}&from=now-7d&to=now"'
     print(graph_cmd)
     os.system(login_cmd)
     sleep(3)
@@ -17,6 +17,3 @@ if __name__ == '__main__':
     get_graph('2926')
 
 
-
-#wget  quiet --save-cookies=./arquivo_cookie --keep-session-cookies 'http://192.168.5.22/zabbix/index.php?login=1&form=1&name=Admin&password=Ch@D0$t0mdt105&enter=ENTER'
-#wget --post-data='' -O ./imagem.png --load-cookies=./arquivo_cookie "http://192.168.5.22/zabbix/chart2.php?graphid=2926&from=now-7d&to=now"
